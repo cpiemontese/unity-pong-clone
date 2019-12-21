ce1ed97ca128108a68dc49b1191b71f86255fa99
Cristiano Piemontese <cristiano.piemontese@gmail.com>
2019-12-21 23:55:01 +0100

    feat: add bouncing ball

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    public float startingVelocityFactor;
+
+    void Awake()
+    {
+        var rb2d = GetComponent<Rigidbody2D>();
+        rb2d.velocity = new Vector2(Random.Range(-2.0f, 2.0f), Random.Range(-1.0f, 1.0f)) * startingVelocityFactor;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public GameObject ball;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Instantiate(ball);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
