812b536166e3bf378f1e3eb98db2a67e847c032c
Cristiano Piemontese <cristiano.piemontese@gmail.com>
2019-12-22 19:33:18 +0100

    feat: add rainbow shadow to title text

---
+++ Assets/Scripts/RainboxText.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class RainboxText : MonoBehaviour
+{
+    public float colorChangeDuration = 1f;
+
+    Text text;
+    Color targetColor;
+    Color currentColor;
+    float timeSinceChange;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        text = GetComponent<Text>();
+        timeSinceChange = 0f;
+        currentColor = text.color;
+        targetColor = Random.ColorHSV();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (timeSinceChange >= colorChangeDuration)
+        {
+            currentColor = targetColor;
+            targetColor = Random.ColorHSV();
+            timeSinceChange = 0f;
+        }
+        timeSinceChange += Time.deltaTime;
+        text.color = Color.Lerp(currentColor, targetColor, timeSinceChange/colorChangeDuration);
+    }
+}
