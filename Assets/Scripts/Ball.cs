84deb7e04779370306ebb8c0df38edb6ab66fa3a
Cristiano Piemontese <cristiano.piemontese@gmail.com>
2019-12-22 18:11:05 +0100

    feat: ball picks up speed if too slow

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -4,12 +4,22 @@ using UnityEngine;
 
 public class Ball : MonoBehaviour
 {
+    Rigidbody2D rb2d;
     public float startingVelocityFactor;
 
     void Awake()
     {
-        var rb2d = GetComponent<Rigidbody2D>();
-        rb2d.velocity = new Vector2(Random.Range(-2.0f, 2.0f), Random.Range(-1.0f, 1.0f)) * startingVelocityFactor;
+        rb2d = GetComponent<Rigidbody2D>();
+        var dir = Random.value;
+        var startingDirection = 0f;
+        if (dir < 0.5f)
+        {
+            startingDirection = -1f;
+        } else
+        {
+            startingDirection = 1f;
+        }
+        rb2d.velocity = new Vector2(startingDirection, Random.Range(-1.0f, 1.0f)) * startingVelocityFactor;
     }
 
     // Start is called before the first frame update
@@ -20,6 +30,10 @@ public class Ball : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Mathf.Abs(rb2d.velocity.x) < startingVelocityFactor)
+        {
+            var sign = Mathf.Sign(rb2d.velocity.x);
+            rb2d.velocity = new Vector2(rb2d.velocity.x + sign*Time.deltaTime, rb2d.velocity.y);
+        }
     }
 }
