9e6286b6ddb04f802480b069edec70c2b5af1087
Cristiano Piemontese <cristiano.piemontese@gmail.com>
2020-05-24 16:30:23 +0200

    difficulty influences ai paddle

--- Assets/Scripts/Paddle/PaddleAIProducer.cs
+++ Assets/Scripts/Paddle/PaddleAIProducer.cs
@@ -1,15 +1,22 @@
 using UnityEngine;
+using System.Collections.Generic;
 
 public class PaddleAIProducer : Producer<Vector2>
 {
+    public GameManager gameManager;
     public float forceFactor = 10.0f;
     public float minActivationDelta = 0.25f;
+    [Range(0f, 1f)]
+    public float easyErrorProbability = 0.5f;
+    [Range(0f, 1f)]
+    public float mediumErrorProbability = 0.25f;
+    [Range(0f, 1f)]
+    public float hardErrorProbability = 0.0f;
 
-    GameManager _gameManager;
     GameObject _ball;
     bool _alreadyGot = false;
 
-    void Start()
+    void Awake()
     {
         _ball = null;
     }
@@ -35,7 +42,7 @@ public class PaddleAIProducer : Producer<Vector2>
 
     override public Vector2 Produce()
     {
-        var newValue = Vector2.zero;
+        var direction = 0;
         var ball = GetBall();
         if (ball != null)
         {
@@ -43,10 +50,41 @@ public class PaddleAIProducer : Producer<Vector2>
             var thisY = transform.position.y;
             var delta = ballY - thisY;
             if (Mathf.Abs(delta) >= minActivationDelta)
-            {
-                newValue = Mathf.Sign(ballY - thisY) > 0 ? Vector2.up : Vector2.down;
-            }
+                direction = Mathf.RoundToInt(Mathf.Sign(delta));
         }
+
+        var directionChangeRoll = Random.Range(0f, 1f);
+        switch (gameManager.difficulty)
+        {
+            case 0:
+                if (directionChangeRoll < easyErrorProbability)
+                {
+                    Debug.Log("Easy: changed direction!");
+                    direction = -direction;
+                }
+                break;
+            case 1:
+                if (directionChangeRoll < mediumErrorProbability)
+                {
+                    Debug.Log("Medium: changed direction!");
+                    direction = -direction;
+                }
+                break;
+            case 2:
+                if (directionChangeRoll < hardErrorProbability)
+                {
+                    Debug.Log("Hard: changed direction!");
+                    direction = -direction;
+                }
+                break;
+            default:
+                break;
+        }
+
+        var newValue = Vector2.zero;
+        if (direction != 0)
+            newValue = Mathf.Sign(direction) > 0 ? Vector2.up : Vector2.down;
+
         return newValue * forceFactor * Time.deltaTime;
     }
 }
